359d394bbb8c162acc6ab7fdab115b674589b691
ThePrimeagen <catspriteee@gmail.com>
2025-03-05 20:51:08 +0300

    Finished!

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 import pygame
+import random
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import *
 from circleshape import CircleShape
+from shot import Shot
 
 def main():
     pygame.init()
@@ -14,13 +16,16 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2   )
-    
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = (updatable)
     asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2   )
 
     dt = 0
 
@@ -34,6 +39,12 @@ def main():
             if asteroid.check_collision(player) == True:
                 print("Game Over!")
                 sys.exit()
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.check_collision(shot) == True:
+                    asteroid.split()
+                    shot.kill()
 
         screen.fill("black")
         for obj in drawable:
