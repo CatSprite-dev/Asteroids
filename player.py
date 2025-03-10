85138b1735f5c64268d032bb2c4493dfe75dacdd
CatSprite-dev <user_423@IEAL-423-M4.>
2025-03-10 14:15:37 +0300

    Update acceleration and inertia

--- player.py
+++ player.py
@@ -8,7 +8,6 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.timer = 0
-        self.speed = PLAYER_SPEED
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -46,25 +45,33 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-            
         if keys[pygame.K_w]:
-            if self.speed < 300:
-                self.speed += PLAYER_ACCELERATION
-            self.move(dt)
-        if keys[pygame.K_w] == 0:
-            if self.speed > 0:
-                self.speed -= PLAYER_ACCELERATION / 1.25
-            self.move(dt)
-        # if keys[pygame.K_s]:
-        #    self.move(-dt)
+            self.move(dt, 1)
+        if keys[pygame.K_s]:
+            self.move(-dt, 1)
+
+        self.velocity *= 0.99
+        self.position += self.velocity * dt
+
+        if self.position.x < 0:
+            self.position.x = SCREEN_WIDTH
+        if self.position.x > SCREEN_WIDTH:
+            self.position.x = 0
+        
+        if self.position.y < 0:
+            self.position.y = SCREEN_HEIGHT
+        if self.position.y > SCREEN_HEIGHT:
+            self.position.y = 0
         
         if keys[pygame.K_SPACE]:
             self.shoot()
         
     
-    def move(self, dt):
+    def move(self, dt, val):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * self.speed * dt
+        self.velocity += forward * PLAYER_ACCELERATION * val
+        self.position += self.velocity * dt
+
 
     def shoot(self):
         if self.timer <= 0:
